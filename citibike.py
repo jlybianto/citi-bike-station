9784b67248792f1f141b052e2c1d4484030d2667
jlybianto <jesse.lybianto@gmail.com>
2015-12-12 18:48:29 -0700

    Import package and pass URL

---
+++ citibike.py
@@ -0,0 +1,5 @@
+# Requests is a package that allows download of data from any online resource.
+import requests
+
+# Pass the URL to point Python to this location.
+r = requests.get("http://www.citibikenyc.com/stations/json")
